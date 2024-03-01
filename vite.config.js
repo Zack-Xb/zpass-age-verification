6151f86e68f22e18def472b12b1ab2250f992e0d
Zack-Xb <zack.xuereb.21@um.edu.mt>
2024-02-29 22:54:22 -0700

    updates vite config

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Resource-Policy': 'same site',
+      'Cross-Origin-Resource-Policy': 'same-site',
     },
   },
 });
